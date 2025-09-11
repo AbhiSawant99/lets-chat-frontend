8293dc5cf5b5a2018e96721d038a871db4b73fba
AbhiSawant99 <sawantabhijeet9990@gmail.com>
2025-09-11 23:09:33 +0530

    fix: for secure connections of websocket as on iphone sockets are not working

--- src/api/socket.api.ts
+++ src/api/socket.api.ts
@@ -3,7 +3,9 @@ import { io } from "socket.io-client";
 
 export const socket = io(BASE_URL, {
   withCredentials: true,
+  transports: ["polling", "websocket"],
   autoConnect: false,
+  secure: true,
 });
 
 socket.on("connect_error", (err) => {
